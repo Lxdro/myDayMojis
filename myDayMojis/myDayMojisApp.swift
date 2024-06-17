1469b76c6ccc4cac7d1e71a9b507f2c4dbb1e304
Lxdro <leandro.tolaini@epita.fr>
2024-06-17 20:51:53 +0800

    Initial Commit

---
+++ myDayMojis/myDayMojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  myDayMojisApp.swift
+//  myDayMojis
+//
+//  Created by Leandro Tolaini on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct myDayMojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myDayMojisTests/myDayMojisTests.swift
@@ -0,0 +1,17 @@
+//
+//  myDayMojisTests.swift
+//  myDayMojisTests
+//
+//  Created by Leandro Tolaini on 17/06/2024.
+//
+
+import Testing
+@testable import myDayMojis
+
+struct myDayMojisTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
